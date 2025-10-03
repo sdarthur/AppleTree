340453f933b8480891e29d4749455f570e06f547
sdarthur <arthur.monte@ufv.br>
2025-10-03 11:49:59 -0300

    working worldgen

--- src/main/java/com/sdarthur/aatm/worldgen/AATMBiomeModifiers.java
+++ src/main/java/com/sdarthur/aatm/worldgen/AATMBiomeModifiers.java
@@ -1,18 +1,28 @@
 package com.sdarthur.aatm.worldgen;
 
 import com.sdarthur.aatm.AnotherAppleTreeMod;
+import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.levelgen.GenerationStep;
 import net.neoforged.neoforge.common.world.BiomeModifier;
+import net.neoforged.neoforge.common.world.BiomeModifiers;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 public class AATMBiomeModifiers {
+    public static final ResourceKey<BiomeModifier> ADD_APPLE_TREE = registerKey("add_apple_tree");
 
     public static void bootstrap(BootstrapContext<BiomeModifier> context) {
         var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
         var biomes = context.lookup(Registries.BIOME);
+
+        context.register(ADD_APPLE_TREE, new BiomeModifiers.AddFeaturesBiomeModifier(
+                HolderSet.direct(biomes.getOrThrow(Biomes.FOREST), biomes.getOrThrow(Biomes.FLOWER_FOREST), biomes.getOrThrow(Biomes.WINDSWEPT_FOREST)),
+                HolderSet.direct(placedFeatures.getOrThrow(AATMPlacedFeatures.APPLE_TREE_PLACED_KEY)),
+                GenerationStep.Decoration.VEGETAL_DECORATION));
     }
 
     private static ResourceKey<BiomeModifier> registerKey(String name) {
--- src/main/java/com/sdarthur/aatm/worldgen/AATMPlacedFeatures.java
+++ src/main/java/com/sdarthur/aatm/worldgen/AATMPlacedFeatures.java
@@ -1,26 +1,38 @@
 package com.sdarthur.aatm.worldgen;
 
 import com.sdarthur.aatm.AnotherAppleTreeMod;
+import com.sdarthur.aatm.block.AATMBlocks;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.placement.PlacementModifier;
+import net.minecraft.world.level.levelgen.placement.RarityFilter;
+
+import java.util.List;
 
 public class AATMPlacedFeatures {
+    public static final ResourceKey<PlacedFeature> APPLE_TREE_PLACED_KEY = registerKey("apple_tree_placed");
 
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
+
+        register(context, APPLE_TREE_PLACED_KEY, configuredFeatures.getOrThrow(AATMConfiguredFeatures.APPLE_TREE_KEY),
+                VegetationPlacements.treePlacement(RarityFilter.onAverageOnceEvery(200),
+                        AATMBlocks.APPLE_TREE_SAPLING.get()));
     }
 
     private static ResourceKey<PlacedFeature> registerKey(String name) {
         return ResourceKey.create(Registries.PLACED_FEATURE, ResourceLocation.fromNamespaceAndPath(AnotherAppleTreeMod.MODID, name));
     }
 
-    private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstrapContext<ConfiguredFeature<?, ?>> context, ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
-        context.register(key, new ConfiguredFeature<>(feature, configuration));
+    private static void register(BootstrapContext<PlacedFeature> context, ResourceKey<PlacedFeature> key, Holder<ConfiguredFeature<?, ?>> configuration,
+                                 List<PlacementModifier> modifiers) {
+        context.register(key, new PlacedFeature(configuration, List.copyOf(modifiers)));
     }
 }
